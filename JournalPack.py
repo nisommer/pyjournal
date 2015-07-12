0f2e6ab3b3c806821226b8fc0669469d38844e84
Nicolas maison win8 <nsom67@free.fr>
2015-07-12 16:03:48 +0200

    Added reading/writing from file with serialization

--- JournalPack.py
+++ JournalPack.py
@@ -1,10 +1,20 @@
-__author__ = 'nicolassommer'
 
+#from __future__ import absolute_import
+#from __future__ import absolute_import, unicode_literals
+#from . import Entry
 from tkinter import *
 from tkinter import ttk
 from datetime import datetime
+
+import Entry
+import methods
+import json
+import jsonpickle
+import os
+
 #import time
 from collections import Counter # To count each word ...
+__author__ = 'nicolassommer'
 
 
 # Count words: OK
@@ -62,12 +72,49 @@ def key_touched(*args):
     wordList.sort(reverse=1)
     cnames.set(tuple(wordList))
 
+def return_touched(*args):
+
+    """
+    Update when a return is typed. Goal is to save the data here ...
+    :param args:
+    """
+
+    print("test")
+    input_str = retrieve_input()
+    newentry = methods.new_entry(input_str)
+    # Print to unicode
+    #print(newentry.__unicode__())
+    # Print to json (trial 1) but harder to get back from the string afterwards !
+    #print(json.dumps(newentry.to_dict()))
+
+    jsondump = jsonpickle.encode(newentry)
+    print("json dump:" + jsondump)
+
+
+
+    myfile = open(journal_file,"w")
+    #myfile = open('data.txt', 'w')
+    myfile.write(jsondump)
+    myfile.close()
+
+
+    # I will need a way to read from json too ...
+    #print(jsonpickle.encode(newentry))
+    #recreated = jsonpickle.decode(jsonpickle.encode(newentry))
+    #print(type(recreated))
+    #print("body: " + recreated.body)
+    #print(recreated.body)
+
+
+    # Here create a new Entry, and print that Entry ...
+
+
 
 
 def display_info():
     # Probably there is a better way than to create everything here, it should just appear...
     t = Toplevel(root)
-    t.geometry('300x200+'+str(root.winfo_width()+root.winfo_x())+'+'+str(root.winfo_y())) # Set it next to the initial window
+    t.geometry('300x200+'+str(root.winfo_width()+root.winfo_x()+20)+'+'+str(root.winfo_y())) # Set it next to the initial window
     print(root.aspect)
     root.winfo_geometry()
     root.wm_geometry()
@@ -76,11 +123,11 @@ def display_info():
     input_str = retrieve_input()
 
     lbox = Listbox(t, listvariable=cnames, height=5)
-
     lbox.pack(fill=BOTH,expand=1)
 
 
 
+### MAIN code
 
 root = Tk()
 root.title("Journal test program")
@@ -129,13 +176,42 @@ progressbar.pack(side=LEFT,expand=1,fill = X)
 quitButton.pack(side=LEFT)
 infoButton.pack(side = RIGHT)
 
+# Set focus on the text area
+text.focus_set()
 
 cnames = StringVar() # Used to display words in the new window
 last_touch = datetime.now()
 
 
+# Get dropbox's location
+dropbox_config_file = open(os.getenv('APPDATA') + "\Dropbox\info.json")
+test = dropbox_config_file.read()
+data = json.loads(test)
+dropbox_path = data["personal"]["path"]
+print("dropbox_path: " + dropbox_path)
+journal_file = dropbox_path+'.journal.txt'
+
+
+# Load data
+try:
+    myfile = open(journal_file)
+    #myfile = open('data.txt')
+    jsondump = myfile.read()
+    #print("read from last time: " +jsondump)
+    myEntry = jsonpickle.decode(jsondump)
+    text.insert("1.0", myEntry.body)
+except:
+    print("File not found, not loading anything ...")
+
+print(os.getenv('APPDATA'))
+
+
+
+
+
 # Set bindings
 root.bind('<KeyPress>', key_touched)
+root.bind('<Return>', return_touched)
 
 # Run the program
 root.mainloop()
---
+++ methods.py
@@ -0,0 +1,54 @@
+import re
+from datetime import datetime
+#from __future__ import absolute_import, unicode_literals
+#import Entry
+import time # does not seem to be working
+
+__author__ = 'Nico'
+import Entry
+
+
+def new_entry(raw, date=None, sort=True):
+        """Constructs a new entry from some raw text input.
+        If a date is given, it will parse and use this, otherwise scan for a date in the input first."""
+
+        raw = raw.replace('\\n ', '\n').replace('\\n', '\n')
+        starred = False
+        # Split raw text into title and body
+        sep = re.search("\n|[\?!.]+ +\n?", raw)
+        title, body = (raw[:sep.end()], raw[sep.end():]) if sep else (raw, "")
+
+        # Overwrite this result:
+        if 1:
+            title = ""
+            body=raw
+
+        print("title: " + title)
+        print("body: " + body)
+
+
+        ## Title parsing is not good : do it manually : take the first line ! or just dont take titles ...
+        starred = False
+        if not date:
+            if title.find(": ") > 0:
+                starred = "*" in title[:title.find(": ")]
+                #date = time.parse(title[:title.find(": ")], default_hour=self.config['default_hour'], default_minute=self.config['default_minute'])
+                if date or starred:  # Parsed successfully, strip that from the raw text
+                    title = title[title.find(": ")+1:].strip()
+            elif title.strip().startswith("*"):
+                starred = True
+                title = title[1:].strip()
+            elif title.strip().endswith("*"):
+                starred = True
+                title = title[:-1].strip()
+        if not date:  # Still nothing? Meh, just live in the moment.
+            #date = time.parse("now")
+            date = datetime.now()# This should be changed
+
+        entry = Entry.Entry( date, title, body, starred=starred)
+        entry.modified = True
+
+        #self.entries.append(entry)
+       # if sort:
+        #    self.sort()
+        return entry
